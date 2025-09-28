c7af335a21ee21bbbc4ff0f7fe4d6c2bdbf22e88
Sofi Dobriak <sofi.dobriak@gmail.com>
2025-09-28 13:59:23 +0300

    feat: add footer

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,16 +1,26 @@
 import Swiper from 'swiper';
 import 'swiper/css';
 import 'swiper/css/navigation';
-import { Navigation } from 'swiper/modules';
+import 'swiper/css/autoplay';
+import { Navigation, Autoplay } from 'swiper/modules';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const mySwiper = new Swiper('.swiper', {
+  const mySwiper = new Swiper('.hero-swiper', {
     modules: [Navigation],
     slidesPerView: 1,
     spaceBetween: 10,
     navigation: {
       nextEl: '.swiper-button.next',
       prevEl: '.swiper-button.prev',
+    },
+  });
+
+  const footerSwiper = new Swiper('.footer-swiper', {
+    modules: [Autoplay],
+    slidesPerView: 1,
+    spaceBetween: 10,
+    autoplay: {
+      delay: 3000,
     },
   });
 });
